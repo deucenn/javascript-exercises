c9861ac75b4e1cdec27b0b53bc2006cf69045726
deucenn <panix.black@web.de>
2024-10-02 14:56:58 +0200

    done with all tasks

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function (num) {
+const fibonacci = (num) => {
     
+    // Always converts num to a number
+    num = Number(num);
+
+    // Returns "OOPS" if num is smaller than zero
+    if (num < 0) return "OOPS"
+    else if (num <= 1) return num;
+
+    let prev = 0, current = 1;
+
+    for (let i = 2; i <= num; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return current;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
